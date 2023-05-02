e1c4f42157ce69ddbf4cf65b8b92501c7c24ef77
rawnly <rawnly@users.noreply.github.com>
2023-05-02 23:10:43 +0200

    feat: turbo + analytics

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "tailwindcss/tailwind.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import { FC } from "react";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default App;
